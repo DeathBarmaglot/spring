73a2f5cadc23d48e892446c5c2dd6fe74d59e5ab
deathbarmaglot <54247708+DeathBarmaglot@users.noreply.github.com>
2022-01-04 13:26:06 +0200

    deploy: heroku plus postgres

--- src/main/java/com/shop/web/config/SecurityConfig.java
+++ src/main/java/com/shop/web/config/SecurityConfig.java
@@ -1,46 +1,46 @@
-        package com.shop.web.config;
-
-import com.shop.web.filter.CustomAuthenticationFilter;
-import com.shop.web.filter.CustomAuthorizationFilter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final UserDetailsService userDetailsService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
-        customAuthenticationFilter.setFilterProcessesUrl("/");
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().antMatchers("/login/**").permitAll();
-        http.authorizeRequests().antMatchers(GET, "/products/add/**").hasAnyAuthority("USER");
-        http.authorizeRequests().antMatchers(POST, "/user/add/**").hasAnyAuthority("ADMIN");
-        http.authorizeHttpRequests().anyRequest().authenticated();
-        http.addFilter(customAuthenticationFilter);
-        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
-}
+//        package com.shop.web.config;
+//
+//import com.shop.web.filter.CustomAuthenticationFilter;
+//import com.shop.web.filter.CustomAuthorizationFilter;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//import static org.springframework.http.HttpMethod.GET;
+//import static org.springframework.http.HttpMethod.POST;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    private final UserDetailsService userDetailsService;
+//    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+//        customAuthenticationFilter.setFilterProcessesUrl("/");
+//        http.csrf().disable();
+//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        http.authorizeRequests().antMatchers("/login/**").permitAll();
+//        http.authorizeRequests().antMatchers(GET, "/products/add/**").hasAnyAuthority("USER");
+//        http.authorizeRequests().antMatchers(POST, "/user/add/**").hasAnyAuthority("ADMIN");
+//        http.authorizeHttpRequests().anyRequest().authenticated();
+//        http.addFilter(customAuthenticationFilter);
+//        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+//    }
+//}
--- src/main/java/com/shop/web/config/WebSecurityConfig.java
+++ src/main/java/com/shop/web/config/WebSecurityConfig.java
@@ -13,28 +13,37 @@
 //@Configuration
 //@EnableWebSecurity
 //public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Autowired
-//    private DataSource dataSource;
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/").permitAll()
-//                .anyRequest().authenticated()
-//        .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .permitAll()
-//        .and()
-//                .logout()
-//                .permitAll();
-//    }
+//   @Autowired
+//   private DataSource dataSource;
+//   @Override
+//   protected void configure(HttpSecurity http) throws Exception {
+//       http
+//               .authorizeRequests()
+//               .antMatchers("/").permitAll()
+//               .anyRequest().authenticated()
+//       .and()
+//               .formLogin()
+//               .loginPage("/login")
+//               .permitAll()
+//       .and()
+//               .logout()
+//               .permitAll();
+//   }
 //
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.jdbcAuthentication().dataSource(dataSource)
-//                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-//                .usersByUsernameQuery("select name, description, email from products where name=?")
-//                .authoritiesByUsernameQuery("select f.name, fr.roles from foods f inner join user_role fr on f.id = fr.food_id where f.name=?");
-//    }
+//   @Override
+//   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//       auth.jdbcAuthentication().dataSource(dataSource)
+//               .passwordEncoder(NoOpPasswordEncoder.getInstance())
+//               .usersByUsernameQuery("select name, description, email from products where name=?")
+//               .authoritiesByUsernameQuery("select f.name, fr.roles from foods f inner join user_role fr on f.id = fr.food_id where f.name=?");
+//   }
+//
+//
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//    http.requiresChannel()
+//      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+//      .requiresSecure();
+//
+//}
 //}
